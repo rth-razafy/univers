f3f3b014d958b1ede8b93fac99014acee4a93210
Herman Razafindrakoto <rth.razafy@gmail.com>
2022-10-09 11:42:43 +0300

    last modify

--- src/app/folder/init-folder/init-folder.component.ts
+++ src/app/folder/init-folder/init-folder.component.ts
@@ -27,22 +27,26 @@ export class InitFolderComponent implements OnInit {
       {
         label: "Nom 1",
         indice: 2451,
-        color: "primary"
+        color: "primary",
+        colorText: "primary"
       },
       {
         label: "Nom 2",
         indice: 2452,
-        color: "primary"
+        color: "primary",
+        colorText: "danger"
       },
       {
         label: "Nom 3",
         indice: 2453,
-        color: "primary"
+        color: "primary",
+        colorText: "secondary"
       },
       {
         label: "Nom 4",
         indice: 2454,
-        color: "primary"
+        color: "primary",
+        colorText: "warning"
       }
     ];
   }
